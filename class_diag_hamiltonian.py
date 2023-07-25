fixed a coding mistake in the effective hamiltonian and added single configuration calculation

class_diag_hamiltonian.py
@@ -6,6 +6,7 @@ import os, sys, gc
 path = os.path.dirname(__file__) 
 sys.path.append(path)
 
+import class_equations_of_motion as eom 
 import class_handle_wavefunctions as h_wavef
 import class_energy as energy
 
@@ -90,7 +91,7 @@ class diagonalization:
         '''
         return np.outer(psi[i1,j1], psi[i2,j2])
     
-    def lagrange_multiplier(self, psi, H_psi):
+    def lagrange_multiplier(self, psi, i, j, H_psi):
         '''
             Computes: Lagrange Multiplier for wavefunction psi
 
@@ -107,7 +108,15 @@ class diagonalization:
         '''
         psi_conj = np.conjugate(psi)
 
-        lag_mul = np.diag(np.einsum('n,nn->n', psi_conj, H_psi))
+        eom_object = eom.eom(params=self.param_dict)
+        eom_object.V_0 = self.V_0 
+
+        H_psi = eom_object.hpsi_lang_firsov(psi)
+        lag_mul = np.einsum('ijk,ijk->ij', psi_conj, H_psi)
+
+        lag_mul = np.diag(lag_mul[i,j]*np.ones(self.n, dtype=complex))
+        
+        #lag_mul = np.diag(np.einsum('n,nn->n', psi_conj, H_psi))
 
         return lag_mul
     
@@ -159,9 +168,9 @@ class diagonalization:
                 transfer terms
                 '''
                 TDr = TD / (TD_arr[i, j]*TD_arr[i-1, j])
-                TUr = TU / (TU_arr[i, j]*TD_arr[(i+1)%self.My, j])
-                TRr = TR / (TR_arr[i, j]*TD_arr[i, j-1])
-                TLr = TL / (TL_arr[i, j]*TD_arr[i, (j+1)%self.Mx])
+                TUr = TU / (TU_arr[i, j]*TU_arr[(i+1)%self.My, j])
+                TRr = TR / (TR_arr[i, j]*TR_arr[i, j-1])
+                TLr = TL / (TL_arr[i, j]*TL_arr[i, (j+1)%self.Mx])
 
                 H_psi -= self.ty*(np.exp(-1j*(2*np.pi*self.qy/self.My))*TDr*self.projector(psi, (i+1)%self.My, j, i-1, j)\
                             + np.exp(+1j*(2*np.pi*self.qy/self.My))*TUr*self.projector(psi, i-1, j, (i+1)%self.My, j))
@@ -183,8 +192,8 @@ class diagonalization:
                 '''
                 Lagrange Multiplier
                 '''
-                lag_mul = self.lagrange_multiplier(psi[i,j], H_psi)
-                H_psi -= lag_mul
+                lag_mul = self.lagrange_multiplier(psi, i, j, H_psi)
+                #H_psi -= lag_mul
 
                 '''
                 diagonalization of h_eff

multi_ref_ci.py
@@ -49,7 +49,13 @@ def plot_ref_densities(params, ferro_order, ferro_domain_v, ferro_domain_h, smal
     plt.show()
     plt.pcolormesh(small_polaron_sigma)
     plt.show()
-    
+
+def plot_energies_during_scf(e_fo, e_fdv, e_fdh, e_sp):
+    plt.plot(e_fo.real); plt.show()
+    plt.plot(e_fdv.real); plt.show()
+    plt.plot(e_fdh.real); plt.show()
+    plt.plot(e_sp.real); plt.show() 
+
 in_object = h_in.params(on_cluster=True) # object for handling inputs from command line
 print('\nParameters input file:')
 params = in_object.get_parameters_imag_time_prop(path+'/', arg=1)
@@ -74,12 +80,13 @@ mult_ref_object = diag_heff.multi_ref_ci(params=params)
 q = np.array([params['qx'],params['qy']])
 
 print_ref_energies(coupl_object, q, ferro_order, ferro_domain_v, ferro_domain_h, small_polaron)
+plot_ref_densities(params, ferro_order, ferro_domain_v, ferro_domain_h, small_polaron)
 
 '''
 Create new Reference Ground States from the SCF method of diagonalizing eff. Hamiltonian
 '''
-iter_number = 5
-mult_ref_object.set_phase_bool = False
+iter_number = 50
+mult_ref_object.set_phase_bool = True
 
 new_ferro_gs, conv_energ_gs_fo, overlap_arr_fo = mult_ref_object.creat_new_ref_state(iter_number, ferro_order, q)
 new_ferro_domain_v, conv_energ_gs_fdv, overlap_arr_fdv = mult_ref_object.creat_new_ref_state(iter_number, ferro_domain_v, q)
@@ -88,6 +95,7 @@ new_small_polaron, conv_energ_gs_sp, overlap_arr_sp = mult_ref_object.creat_new_
 
 print_ref_energies(coupl_object, q, new_ferro_gs, new_ferro_domain_v, new_ferro_domain_h, new_small_polaron)
 plot_ref_densities(params, new_ferro_gs, new_ferro_domain_v, new_ferro_domain_h, new_small_polaron)
+plot_energies_during_scf(conv_energ_gs_fo, conv_energ_gs_fdv, conv_energ_gs_fdh, conv_energ_gs_sp)
 
 '''
 Diagonalize effective Hamiltonians to get excited states