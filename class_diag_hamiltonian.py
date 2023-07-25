52e10fd3a2312cd7f63bacaa08cec1a6fa9e041b
Florian KLUIBENSCHEDL <fkluiben@lngrad527.ist.local>
2023-07-25 10:38:36 +0200

    added the muti reference ci - though still some problems there

--- class_diag_hamiltonian.py
+++ class_diag_hamiltonian.py
@@ -131,10 +131,33 @@ class diagonalization:
                 elif i == 0 and j == (self.Mx-1): 
                     H_psi += np.diag(self.V_0*np.cos(self.x+0.75*np.pi))
 
+                energ_object = energy.energy(params=self.param_dict)
+                energ_object.qx = 0
+                energ_object.qy = 0
+
+                e_psi = energ_object.calc_energy(psi)[0]
+
+                #H_psi -= np.diag(e_psi*np.ones(self.n, dtype=complex))
+
+                mul_left = np.diag(np.ones(self.n, dtype=complex)) - np.outer(psi[i,j], psi[i,j])
+                #H_psi = mul_left@H_psi
+
+                lagrange_multiplier = np.conjugate(psi[i,j])*H_psi #@psi[i,j]  # np.einsum('ijk,ijk->ij', np.conjugate(psi), H_psi)
+                #print(lagrange_multiplier)
+                sign = np.sum((1/(self.n)**0.5)*psi)
+                sign = sign/np.abs(sign)
+                #return np.conjugate(sign)*psi
+                #print(sign)
+                lag_mul = np.diag(np.einsum('n,nn->n',np.conjugate(psi[i,j].T),H_psi))
+                #print(lagrange_multiplier.shape)
+                #print(H_psi.shape)
+                #print(lag_mul.shape)
+                #H_psi -= lagrange_multiplier #e_psi*np.ones(self.n, dtype=complex) #lagrange_multiplier #* np.einsum('n,nn->n', lag_mul, np.outer(psi[i,j],psi[i,j])) #np.diag(psi[i,j]) #e_psi*(1/self.n**0.5)*np.diag(np.ones(self.n, dtype=complex)) #e_psi*np.outer(psi[i,j], psi[i,j]) #np.diag(lagrange_multiplier*np.ones(self.n, dtype=complex)) # [:, :, np.newaxis] * psi_collection
+
                 '''
                 diagonalization of h_eff
                 '''
-                eigen_values, eigen_vector = np.linalg.eigh(H_psi) 
+                eigen_values, eigen_vector = np.linalg.eigh(H_psi)
                 order = np.argsort(eigen_values)
                 eigen_vector = eigen_vector[:,order]
                 eigen_values = eigen_values[order]
@@ -148,3 +171,230 @@ class diagonalization:
                     energy_exc_states[i,j,n] = eigen_values[n]
 
         return energy_exc_states, psi_exc_states
+    
+class multi_ref_ci:
+    def __init__(self, params):
+        self.param_dict = params
+        self.Mx  = int(params['Mx'])
+        self.My  = int(params['My'])
+        self.M   = int(params['Mx']*params['My'])
+        self.B   = float(params['B'])
+        self.V_0 = 0 if isinstance(params['V_0'], list) == True else float(params['V_0']) # for safety, update, set outside!
+        self.tx  = float(params['tx'])
+        self.ty  = float(params['ty'])
+        self.qx  = int(params['qx'])
+        self.qy  = int(params['qy'])
+        self.exc_states = int(params['excitation_no'])
+        self.n   = int(params['n'])
+        self.x   = (2*np.pi/self.n)*np.arange(self.n) # make phi (=angle) grid
+        self.set_phase_bool = True # bool to control whether to set phase or not
+
+    def set_phase(self, psi):
+        '''
+            Computes: phase of the wavefunction psi
+
+            ----
+            Inputs:
+                psi (3-dimensional: (My,Mx,n), dtype=complex)
+            ----
+
+            ----
+            Outputs:
+                psi: though phase set to 1 if self.set_phase_bool == True (global class flag, set outside!)
+            ----
+        '''
+
+        psi_man = psi.copy() 
+
+        if self.set_phase_bool == True:
+            sign = np.sum((1/self.n**0.5)*psi_man)
+            sign = sign/np.abs(sign)
+
+            psi_man = np.conjugate(sign)*psi_man
+
+        return psi_man
+
+    def get_indices_for_rotor_ij(self, i, j, index_list_rotor_1, index_list_rotor_2):
+        '''
+            Computes: index list with which the i,j-th rotor is interacting
+
+            ----
+            Inputs:
+                i (scalar, int): rotor index along My axis
+                j (scalar, int): rotor index along Mx axis
+                index_list_rotor_1 (list, dtype=int): a succession of (i,j)-pairs, in the end contains the 
+                    first rotors that interact with the ones of index_list_rotor_2
+                index_list_rotor_2 (list, dtype=int): a successon of (k,p)-pairs, with which the rotor 1 interacts 
+            ----
+
+            ----
+            Outputs:
+                index_list_rotor_1
+                index_list_rotor_2
+            ----
+        '''
+        for p in range(j+1,self.Mx):
+            index_arr_rotor_1 = np.array([i,j])
+            index_arr_rotor_2 = np.array([i,p])
+
+            index_list_rotor_1.append(index_arr_rotor_1)
+            index_list_rotor_2.append(index_arr_rotor_2)
+
+        for k in range(i+1,self.My):
+            for p in range(self.Mx):
+                index_arr_rotor_1 = np.array([i,j])
+                index_arr_rotor_2 = np.array([k,p])
+
+                index_list_rotor_1.append(index_arr_rotor_1)
+                index_list_rotor_2.append(index_arr_rotor_2)
+
+        return index_list_rotor_1, index_list_rotor_2
+
+    def get_double_rotor_excitation_list(self):
+        '''
+            Computes: computes index lists of rotor pairs that interact with one another
+
+            ----
+            Inputs:
+                None
+            ----
+
+            ----
+            Outputs:
+                index_list_rotor_1 (list with M*(M-1)/2 entries): rotor 1 indices that "interact" with rotor 2
+                index_list_rotor_2 (list with M*(M-1)/2 entries): rotor 2 indices that "interact" with the ones of rotor 1
+            ----
+        '''
+        index_list_rotor_1 = []
+        index_list_rotor_2 = []
+        for i in range(self.My):
+            for j in range(self.Mx):
+                index_list_rotor_1, index_list_rotor_2 = self.get_indices_for_rotor_ij(i, j, index_list_rotor_1, index_list_rotor_2)
+
+        return index_list_rotor_1, index_list_rotor_2
+
+    def creat_new_ref_state(self, iter_number, ref_state, q):
+        '''
+            Computes: new reference state to ensure orthogonality among excited states and reference ground state
+
+            ----
+            Inputs:
+                iter_number (int): number of iterations of self-consistent effective hamiltonian diagonalization
+                ref_state (3-dimensional: (My,Mx,n)): reference ground state
+                q (2-dimensional: (qx, qy)): momentum numbers of ref. ground state
+            ----
+
+            ----
+            Description:
+                1. Compute effective Hamiltonian and diagonalize it
+                2. Create new reference ground state with the ground states of the diagonalizatoin
+                3. Compute energy and overlap of the new ref ground state
+                4. Repeat
+            ----
+
+            ----
+            Outputs:
+                new_ref (3-dimensional: (My,Mx,n)): new reference ground state
+                conv_energ_gs (1-dimensional: (iter_number)): energies during the convergence
+                overlap_arr (1-dimensional: (iter_number)): overlaps with previous state during the convergence
+            ----
+        '''
+
+        new_ref = ref_state.copy()
+        
+        coupl_object = energy.coupling_of_states(params=self.param_dict)
+        diag_object = diagonalization(params=self.param_dict)
+
+        overlap_arr = np.zeros(iter_number, dtype=complex)
+        conv_energ_gs = np.zeros(iter_number, dtype=complex)
+
+        for t in range(iter_number):
+            new_ref_next = new_ref.copy()
+            energy_exc_states, psi_exc_states = diag_object.diag_h_eff(new_ref_next)
+
+            for i in range(self.My):
+                for j in range(self.Mx):
+                    new_ref[i,j] = self.set_phase(psi_exc_states[i,j,0]) 
+        
+            conv_energ_gs[t] = coupl_object.calc_hamiltonian_matrix_element(new_ref, q, new_ref, q)[0]
+            overlap_arr[t] = coupl_object.calc_overlap(new_ref, new_ref_next)
+            print('Iter =', t, ', Overlap =', overlap_arr[t])
+    
+        return new_ref, conv_energ_gs, overlap_arr
+
+    def append_single_excitation(self, ref_gs, psi_arr, psi_exc_states):
+        '''
+            Computes: single-excitation list
+
+            ----
+            Inputs:
+                ref_gs (3-dimensional: (My,Mx,n)): reference ground state
+                psi_arr (list): list of wavefunctions
+                psi_excited_states (4-dimensional: (My,Mx,exc_states,n)): contains the excitation wavefunctions for every rotor 
+            ----
+
+            ----
+            Description:
+                Number of single-rotor excitations:
+                    self.exc_states*self.My*self.Mx
+
+                For every excitation number (max: self.exc_states), create a wavefunction wavefunction with 
+                the i,j-th rotor excited in the respectively chosen excited state
+            ----
+
+            ----
+            Outputs:
+                psi_arr: updated with the single-rotor excitation wavefunctions
+            ----
+        '''
+        for m in range(1,self.exc_states):
+            for i in range(self.My):
+                for j in range(self.Mx):
+                    psi = ref_gs.copy() # to avoid pointer issues
+                    psi[i,j] = self.set_phase(psi_exc_states[i,j,m]) 
+
+                    psi_arr.append(psi)
+
+        return psi_arr
+
+    def append_double_excitations(self, ref_gs, psi_arr, psi_exc_states):
+        '''
+            Computes: double-excitation list
+
+            ----
+            Inputs:
+                ref_gs (3-dimensional: (My,Mx,n)): reference ground state
+                psi_arr (list): list of wavefunctions
+                psi_excited_states (4-dimensional: (My,Mx,exc_states,n)): contains the excitation wavefunctions for every rotor 
+            ----
+
+            ----
+            Description:
+                Number of double-rotor excitations: from combinatorics:
+                    self.exc_states**2*(self.My*self.Mx*(self.My*self.Mx-1)/2)
+
+                For every excitation number (max: self.exc_states), create a wavefunction wavefunction with 
+                the i,j-th rotor excited in the respectively chosen excited state
+            ----
+
+            ----
+            Outputs:
+                psi_arr: updated with the double-rotor excitation wavefunctions
+            ----
+        '''
+        index_list_rotor_1, index_list_rotor_2 = self.get_double_rotor_excitation_list()
+        inter_combinations = len(index_list_rotor_1)
+        for m1 in range(1,self.exc_states):
+            for m2 in range(1,self.exc_states):
+                for i in range(inter_combinations):
+                    index_rotor_1 = index_list_rotor_1[i]
+                    index_rotor_2 = index_list_rotor_2[i]
+
+                    psi = ref_gs.copy()
+
+                    psi[index_rotor_1[0],index_rotor_1[1]] = self.set_phase(psi_exc_states[index_rotor_1[0],index_rotor_1[1],m1]) 
+                    psi[index_rotor_2[0],index_rotor_2[1]] = self.set_phase(psi_exc_states[index_rotor_2[0],index_rotor_2[1],m2]) 
+
+                    psi_arr.append(psi)
+
+        return psi_arr
---
+++ multi_ref_ci.py
@@ -0,0 +1,126 @@
+import numpy as np
+
+import matplotlib.pyplot as plt
+import os, sys, csv, time
+
+import gc
+
+path = os.path.dirname(__file__) 
+sys.path.append(path)
+
+# import user-defined classes
+import class_diag_hamiltonian as diag_heff
+import class_energy as energy
+import class_mass_size as mass_size
+import class_handle_input as h_in
+import class_visualization as vis
+import class_handle_wavefunctions as h_wavef
+
+def print_ref_energies(coupl_object, q, ferro_order, ferro_domain_v, ferro_domain_h, small_polaron):
+    e_order = coupl_object.calc_hamiltonian_matrix_element(ferro_order, q, ferro_order, q)[0]
+    e_d_v = coupl_object.calc_hamiltonian_matrix_element(ferro_domain_v, q, ferro_domain_v, q)[0]
+    e_d_h = coupl_object.calc_hamiltonian_matrix_element(ferro_domain_h, q, ferro_domain_h, q)[0]
+    e_s = coupl_object.calc_hamiltonian_matrix_element(small_polaron, q, small_polaron, q)[0]
+
+    print('\nE (Ferro Order)      =', e_order)
+    print('E (Ferro Domain Vert.) =', e_d_v)
+    print('E (Ferro Domain Hor.)  =', e_d_h)
+    print('E (Small Polaron)      = ', e_s, '\n')
+
+def plot_ref_densities(params, ferro_order, ferro_domain_v, ferro_domain_h, small_polaron):
+    polaron_size_object = mass_size.polaron_size(params=params)
+
+    ferro_order_sigma = polaron_size_object.calc_polaron_size(ferro_order, '1')
+    ferro_domain_v_sigma = polaron_size_object.calc_polaron_size(ferro_domain_v, '1')
+    ferro_domain_h_sigma = polaron_size_object.calc_polaron_size(ferro_domain_h, '1')
+    small_polaron_sigma = polaron_size_object.calc_polaron_size(small_polaron, '1')
+
+    plt.pcolormesh(ferro_order_sigma)
+    plt.show()
+    plt.pcolormesh(ferro_domain_v_sigma)
+    plt.show()
+    plt.pcolormesh(ferro_domain_h_sigma)
+    plt.show()
+    plt.pcolormesh(small_polaron_sigma)
+    plt.show()
+    
+in_object = h_in.params(on_cluster=True) # object for handling inputs from command line
+print('\nParameters input file:')
+params = in_object.get_parameters_imag_time_prop(path+'/', arg=1)
+
+'''
+load wavefunctions
+'''
+path_wavefunction_1 = params['path1']
+path_wavefunction_2 = params['path2']
+path_wavefunction_3 = params['path3']
+path_wavefunction_4 = params['path4']
+
+ferro_order    = np.load(path+'/'+path_wavefunction_1)
+ferro_domain_v = np.load(path+'/'+path_wavefunction_2)
+ferro_domain_h = np.load(path+'/'+path_wavefunction_3)
+small_polaron  = np.load(path+'/'+path_wavefunction_4)
+
+coupl_object = energy.coupling_of_states(params=params)
+diag_object = diag_heff.diagonalization(params=params)
+mult_ref_object = diag_heff.multi_ref_ci(params=params)
+
+q = np.array([params['qx'],params['qy']])
+
+print_ref_energies(coupl_object, q, ferro_order, ferro_domain_v, ferro_domain_h, small_polaron)
+
+'''
+Create new Reference Ground States from the SCF method of diagonalizing eff. Hamiltonian
+'''
+iter_number = 5
+mult_ref_object.set_phase_bool = False
+
+new_ferro_gs, conv_energ_gs_fo, overlap_arr_fo = mult_ref_object.creat_new_ref_state(iter_number, ferro_order, q)
+new_ferro_domain_v, conv_energ_gs_fdv, overlap_arr_fdv = mult_ref_object.creat_new_ref_state(iter_number, ferro_domain_v, q)
+new_ferro_domain_h, conv_energ_gs_fdh, overlap_arr_fdh = mult_ref_object.creat_new_ref_state(iter_number, ferro_domain_h, q)
+new_small_polaron, conv_energ_gs_sp, overlap_arr_sp = mult_ref_object.creat_new_ref_state(iter_number, small_polaron, q)
+
+print_ref_energies(coupl_object, q, new_ferro_gs, new_ferro_domain_v, new_ferro_domain_h, new_small_polaron)
+plot_ref_densities(params, new_ferro_gs, new_ferro_domain_v, new_ferro_domain_h, new_small_polaron)
+
+'''
+Diagonalize effective Hamiltonians to get excited states
+'''
+energy_exc_states, ferro_order_exc_states = diag_object.diag_h_eff(new_ferro_gs)
+energy_exc_states, ferro_domain_v_exc_states = diag_object.diag_h_eff(new_ferro_domain_v)
+energy_exc_states, ferro_domain_h_exc_states = diag_object.diag_h_eff(new_ferro_domain_h)
+energy_exc_states, small_polaron_exc_states = diag_object.diag_h_eff(small_polaron)
+
+'''
+Create list of wavefunctions containing GS, Single-, and Double-Excitations
+'''
+psi_arr = []
+
+psi_arr.append(new_ferro_gs)
+psi_arr.append(new_ferro_domain_v)
+psi_arr.append(new_ferro_domain_h)
+psi_arr.append(new_small_polaron)
+
+psi_arr = mult_ref_object.append_single_excitation(new_ferro_gs, psi_arr, ferro_order_exc_states)
+psi_arr = mult_ref_object.append_single_excitation(new_ferro_domain_v, psi_arr, ferro_domain_v_exc_states)
+psi_arr = mult_ref_object.append_single_excitation(new_ferro_domain_h, psi_arr, ferro_domain_h_exc_states)
+psi_arr = mult_ref_object.append_single_excitation(new_small_polaron, psi_arr, small_polaron_exc_states)
+
+psi_arr = mult_ref_object.append_double_excitations(new_ferro_gs, psi_arr, ferro_order_exc_states)
+psi_arr = mult_ref_object.append_double_excitations(new_ferro_domain_v, psi_arr, ferro_domain_v_exc_states)
+psi_arr = mult_ref_object.append_double_excitations(new_ferro_domain_h, psi_arr, ferro_domain_h_exc_states)
+psi_arr = mult_ref_object.append_double_excitations(new_small_polaron, psi_arr, small_polaron_exc_states)
+
+n_states = len(psi_arr)
+q_arr = np.zeros((n_states,2), dtype=complex)
+
+single_exc_number = (params['excitation_no']-1)*params['My']*params['My']
+double_exc_number = (params['excitation_no']-1)**2*(params['My']*params['My']*(params['My']*params['My']-1)/2)
+print("No. of states =", n_states, \
+      ", No. in theory = 4*(1 +", single_exc_number, "+", double_exc_number, ") =", 4*(1+single_exc_number+double_exc_number))
+
+'''
+Compute the effective Hamiltonian
+'''
+h_eff, s_ove = coupl_object.calc_hamiltonian(n_states, psi_arr, q_arr)
+print("Finished calculation of Hamiltonian!")
