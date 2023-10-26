ac6d34f9f1885b9ff5b072efaccd5706c2e604f1
Florian KLUIBENSCHEDL <fkluiben@lngrad527.ist.local>
2023-10-27 00:08:40 +0200

    new eff mass scan for small couplings

--- eff_mass.py
+++ eff_mass.py
@@ -101,7 +101,7 @@ except:
 '''
 sum_tx_ty = 200
 diff_tx_ty = np.array([0]) #np.array([-20,0,20])
-V_0_arr = np.array([0,0.2,0.4,0.6]) #np.array([0,0.2,0.4,0.6,2,4,6,20,40,60]) # np.arange(0,200,50)
+V_0_arr = np.array([0,0.02,0.04,4,6,20]) #np.array([0,0.2,0.4,0.6,2,4,6,20,40,60]) # np.arange(0,200,50)
 
 print('V_0 =', V_0_arr, '\n')
 for i in diff_tx_ty:
@@ -117,9 +117,9 @@ if calc_flag == 'fo':
     params["My"] = 64
     folder = path+'/results/eff_mass/fo/'
     
-    #params["qx"] = 0
-    #params["qy"] = 0
-    #make_scan(diff_tx_ty, sum_tx_ty, V_0_arr, params, folder)
+    params["qx"] = 0
+    params["qy"] = 0
+    make_scan(diff_tx_ty, sum_tx_ty, V_0_arr, params, folder)
 
     params["qx"] = 1
     params["qy"] = 0
