aee5e82969da4554d5648e635166464e138887e8
Florian KLUIBENSCHEDL <fkluiben@lngrad527.ist.local>
2023-12-19 11:02:12 +0100

    ...

--- eff_mass.py
+++ eff_mass.py
@@ -101,7 +101,7 @@ except:
 '''
 sum_tx_ty = 200
 diff_tx_ty = np.array([100]) #np.array([100]) #np.array([-20,0,20])
-V_0_arr = np.array([1100,1200]) #np.array([50,100,150,200,250]) #np.array([170]) #np.array([2,4,20,50]) #np.array([100]) #np.array([2,4,20,40,60,80,100]) # np.array([0.02,0.04,4,6,20]) #np.array([0,0.2,0.4,0.6,2,4,6,20,40,60]) # np.arange(0,200,50)
+V_0_arr = np.array([1300,1400]) #np.array([50,100,150,200,250]) #np.array([170]) #np.array([2,4,20,50]) #np.array([100]) #np.array([2,4,20,40,60,80,100]) # np.array([0.02,0.04,4,6,20]) #np.array([0,0.2,0.4,0.6,2,4,6,20,40,60]) # np.arange(0,200,50)
 
 print('V_0 =', V_0_arr, '\n')
 for i in diff_tx_ty:
